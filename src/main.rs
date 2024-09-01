8c70f6095236fe0317c8253f555509684d2bc14c
Andrii Fylypiuk <fakeandrii@icloud.com>
2024-09-01 22:05:16 +0200

    Removed unused dependency

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::sync::{Arc, Mutex};
 
+use kernel_notify::network::BlockingNetworkExecutor;
 use kernel_notify::notificator::ConsoleNotificator;
 use kernel_notify::scheduler;
 use kernel_notify::service::factory::NetworkServiceFactory;
 use kernel_notify::service::factory::RockyLinux9_4Factory;
-use kernel_notify::{network::BlockingNetworkExecutor, service::Service};
 
 fn main() {
     let executor = Arc::new(Mutex::new(BlockingNetworkExecutor::default()));
