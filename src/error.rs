86a0c71b4097c8970554817886b18365416792d4
Andrii Fylypiuk <fakeandrii@icloud.com>
2024-09-01 20:50:12 +0200

    FMT Applied

--- src/error.rs
+++ src/error.rs
@@ -4,7 +4,7 @@ use crate::network::NetworkError;
 pub enum Error {
     NetworkError(crate::network::NetworkError),
     GenericError(String),
-    ParsingError(String)
+    ParsingError(String),
 }
 
 impl From<String> for Error {
